d21b8fcb5e2c189d6973bafb2021ea7bd215b9bb
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2022-06-02 14:39:27 -0300

    cambios 1/2

--- assets/js/aviso-cookies.js
+++ assets/js/aviso-cookies.js
@@ -1,27 +1,41 @@
-const switchCookiesOk = document.getElementById('cookies-not-ok-button');
-const switchCookiesNotOk = document.getElementById('cookies-ok-button');
-const cookiesOkBar = document.getElementById('cookies-not-ok');
-const cookiesNotOkBar = document.getElementById('cookies-ok');
 
-if(localStorage.getItem('cookies-aceptadas')){
-	cookiesNotOkBar.style.bottom = '-150px';
-	cookiesOkBar.style.bottom = '0px';
-}
-else{
-	cookiesNotOkBar.style.bottom = '0px';
-	cookiesOkBar.style.bottom = '-150px';
-}
 
-switchCookiesOk.addEventListener('click', e => {
-	e.preventDefault();
-	console.log("beep")
-	localStorage.removeItem('cookies-aceptadas');
-	cookiesOkBar.style.bottom = '0px';
-	cookiesNotOkBar.style.bottom = '-150px';
-});
-switchCookiesNotOk.addEventListener('click', e => {
-	e.preventDefault();
-	localStorage.setItem('cookies-aceptadas', 'true');
-	cookiesOkBar.style.bottom = '-150px';
-	cookiesNotOkBar.style.bottom = '0px';
+window.addEventListener("load", e => {
+
+	console.log("Cargado");
+
+	setTimeout(() => {
+
+
+		const switchCookiesOk = document.getElementById('cookies-not-ok-button');
+		const switchCookiesNotOk = document.getElementById('cookies-ok-button');
+		const cookiesOkBar = document.getElementById('cookies-not-ok');
+		const cookiesNotOkBar = document.getElementById('cookies-ok');
+
+		if (localStorage.getItem('cookies-aceptadas')) {
+			cookiesNotOkBar.style.bottom = '-150px';
+			cookiesOkBar.style.bottom = '0px';
+		}
+		else {
+			cookiesNotOkBar.style.bottom = '0px';
+			cookiesOkBar.style.bottom = '-150px';
+		}
+	
+	
+		switchCookiesOk.addEventListener('click', e => {
+			e.preventDefault();
+			console.log("beep")
+			localStorage.removeItem('cookies-aceptadas');
+			cookiesOkBar.style.bottom = '0px';
+			cookiesNotOkBar.style.bottom = '-150px';
+		});
+		switchCookiesNotOk.addEventListener('click', e => {
+			e.preventDefault();
+			localStorage.setItem('cookies-aceptadas', 'true');
+			cookiesOkBar.style.bottom = '-150px';
+			cookiesNotOkBar.style.bottom = '0px';
+		})
+
+	}, 1000)
+
 })
